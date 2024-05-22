e46e61301675b0a2730b05ff618989a6cfa146d8
ziogenorwekh <ziogenorwekh@gmail.com>
2024-05-22 17:46:25 +0900

    init

---
+++ src/test/java/com/terrakorea/assignment/AssignmentApplicationTests.java
@@ -0,0 +1,15 @@
+package com.terrakorea.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentApplicationTests {
+
+
+	// 1. load cpu status
+	@Test
+	public void getMyCpuStatus() {
+		
+	}
+}
